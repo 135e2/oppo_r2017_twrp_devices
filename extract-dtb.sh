0bda1b83e57109fa9b17c8d840e30f5bf3d58df5
135e2 <shangshangzikun@gmail.com>
2020-07-21 13:08:39 +0800

    test:use extract-dtb.py and dtc to extract dts files
    Try to use https://github.com/PabloCastellano/extract-dtb and dtc to extract dts files
    How to extract:source ./extract-dtb.sh && source ./extract-dts.sh

---
+++ extract-dtb.sh
@@ -0,0 +1,3 @@
+./extract-dtb.py -n ./dt.img
+./extract-dtb.py ./dt.img -o ./dtb
+ls -l ./dtb
---
+++ extract-dts.sh
@@ -0,0 +1,7 @@
+dtc -I dtb -O dts /home/shang/Downloads/oppo_r2017_twrp_devices/dtb/01_dtbdump_Qualcomm_MSM_8926_QRD_SKUG.dtb > ./dts/01_dtbdump_Qualcomm_MSM_8926_QRD_SKUG.dts
+dtc -I dtb -O dts /home/shang/Downloads/oppo_r2017_twrp_devices/dtb/02_dtbdump_Qualcomm_MSM_8926_QRD_SKUG.dtb > ./dts/02_dtbdump_Qualcomm_MSM_8926_QRD_SKUG.dts
+dtc -I dtb -O dts /home/shang/Downloads/oppo_r2017_twrp_devices/dtb/03_dtbdump_Qualcomm_MSM_8926_QRD_SKUG.dtb > ./dts/03_dtbdump_Qualcomm_MSM_8926_QRD_SKUG.dts
+dtc -I dtb -O dts /home/shang/Downloads/oppo_r2017_twrp_devices/dtb/04_dtbdump_Qualcomm_MSM_8926_QRD_SKUG.dtb > ./dts/04_dtbdump_Qualcomm_MSM_8926_QRD_SKUG.dts
+dtc -I dtb -O dts /home/shang/Downloads/oppo_r2017_twrp_devices/dtb/05_dtbdump_Qualcomm_MSM_8926_QRD_SKUG.dtb > ./dts/05_dtbdump_Qualcomm_MSM_8926_QRD_SKUG.dts
+dtc -I dtb -O dts /home/shang/Downloads/oppo_r2017_twrp_devices/dtb/06_dtbdump_Qualcomm_MSM_8926_QRD_SKUG.dtb > ./dts/06_dtbdump_Qualcomm_MSM_8926_QRD_SKUG.dts
+dtc -I dtb -O dts /home/shang/Downloads/oppo_r2017_twrp_devices/dtb/07_dtbdump_Qualcomm_MSM_8926_QRD_SKUG.dtb > ./dts/07_dtbdump_Qualcomm_MSM_8926_QRD_SKUG.dts
